38780123e0626f8cfc9315f56e6039df0653a90e
GustavoSFer <gustavo.sfernander@gmail.com>
2025-06-16 21:19:19 -0300

    feat: Criado a entidade cliente

---
+++ src/main/java/br/com/fernandes/entities/Cliente.java
@@ -0,0 +1,62 @@
+package br.com.fernandes.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cliente implements Serializable {
+
+    private long id;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Cliente(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id == cliente.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
