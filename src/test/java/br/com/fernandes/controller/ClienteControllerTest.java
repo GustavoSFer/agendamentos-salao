94f63c960aa5dacd3bc69d586fd7570eb8ab7b8d
GustavoSFer <gustavo.sfernander@gmail.com>
2025-06-19 16:51:06 -0300

    Test: Criado o teste de criacao de cliente com sucesso no service

--- src/test/java/br/com/fernandes/controller/ClienteControllerTest.java
+++ src/test/java/br/com/fernandes/controller/ClienteControllerTest.java
@@ -3,6 +3,7 @@ package br.com.fernandes.controller;
 import br.com.fernandes.dto.ClienteDTO;
 import br.com.fernandes.entities.Cliente;
 import br.com.fernandes.service.ClienteService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -48,5 +49,17 @@ class ClienteControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.nome").value("Gustavo Fernandes"));
+    }
+
+    @Test
+    @DisplayName("Deve retornar 400 quando falta algum parametro no body")
+    public void deveRetornarFalhaComBodyErradoTest() throws Exception {
+        ClienteDTO clienteDTO = new ClienteDTO("Gustavo Fernandes", "11856952245", null);
+
+        mockMvc.perform(post("/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(clienteDTO)))
+                .andExpect(status().isBadRequest());
+
     }
 }
