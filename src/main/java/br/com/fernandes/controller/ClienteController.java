101ffec3e3045291718c9d6daf03e664a6aa2978
GustavoSFer <gustavo.sfernander@gmail.com>
2025-06-19 16:24:15 -0300

    Test: Criado teste de sucesso de criacao de cliente

--- src/main/java/br/com/fernandes/controller/ClienteController.java
+++ src/main/java/br/com/fernandes/controller/ClienteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
