06d9b3ffde0df8b22e3b8b059a8b8ace3925d692
GustavoSFer <gustavo.sfernander@gmail.com>
2025-06-21 17:26:22 -0300

    fix: importe nao sendo usado

--- src/main/java/br/com/fernandes/dto/ServicoDTO.java
+++ src/main/java/br/com/fernandes/dto/ServicoDTO.java
@@ -2,7 +2,6 @@ package br.com.fernandes.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record ServicoDTO(
