2f8d434a567c4d81e54fac5eadb3c41a7f4ba033
GustavoSFer <gustavo.sfernander@gmail.com>
2025-06-23 21:06:09 -0300

    Test: Dese ser possivel cadastrar um serviço sem a descrição

--- src/test/java/br/com/fernandes/controller/ServicoControllerTest.java
+++ src/test/java/br/com/fernandes/controller/ServicoControllerTest.java
@@ -3,6 +3,7 @@ package br.com.fernandes.controller;
 import br.com.fernandes.dto.ServicoDTO;
 import br.com.fernandes.entities.Servico;
 import br.com.fernandes.service.ServicoService;
+import br.com.fernandes.util.ServicoMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -46,6 +48,22 @@ class ServicoControllerTest {
                 .andExpect(jsonPath("$.nome").value("Corte cabelo masculino"))
                 .andExpect(jsonPath("$.descricao").value(servico.getDescricao()))
                 .andExpect(jsonPath("$.preco").value(servico.getPreco()));
+    }
 
+    @Test
+    @DisplayName("Dese ser possivel cadastrar um serviço sem a descrição")
+    void testCadastrarServicoSemDescricao() throws Exception {
+        ServicoDTO servicoDto = new ServicoDTO("Corte cabelo masculino", null,40.00D);
+        Servico servico = new Servico("Corte cabelo masculino", 40.00D);
+
+        when(servicoService.criarServico(any(Servico.class))).thenReturn(servico);
+
+        mockMvc.perform(post("/servicos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(servicoDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.nome").value(servico.getNome()))
+                .andExpect(jsonPath("$.descricao").value(nullValue()))
+                .andExpect(jsonPath("$.preco").value(servico.getPreco()));
     }
 }
